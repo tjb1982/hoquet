9f34064120b7d6000d1f2dc38e4494a6e0ddecf0
Tom Brennan <tjb1982@gmail.com>
2020-06-12 09:24:57 -0400

    update readme

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # hoquet
-Simple JavaScript HTML/XML templating using Arrays as S-expressions (loosely based on Clojure's Hiccup).
+Minimal web components framework with JavaScript HTML/XML templating using Arrays as S-expressions (loosely based on Clojure's Hiccup).
+
+## Basic usage
 
 ```javascript
 import { render } from "./hoquet.js";
 
 const things = ["bread", "milk", "eggs"];
 render(
-  ["ul", {class: ["things", "list"]}
-  , things.map(x => ["li", x])
-  ]
+  ["ul", {class: ["things", "list"]},
+   things.map(x => ["li", x])]
 );
 // <ul class="things list"><li>bread</li><li>milk</li><li>eggs</li></ul>
 
@@ -18,9 +19,93 @@ render(
 // <link rel="stylesheet" href="styles.css" />
 ```
 
+## Web component mixin
+
+```javascript
+import Hoquet from "@pojagi/hoquet/mixin";
+
+
+class TodoList extends Hoquet(HTMLElement) {
+
+    // Reflect attributes as properties
+    static get reflectedAttributes() { return ["placeholder"]; }
+
+    // Update internal attributes
+    static get observedAttributes() { return ["placeholder"]; }
+    attributeChangedCallback(key, prev, curr) {
+        if (key === "placeholder") {
+            this.$["new-todo"].placeholder = curr;
+        }
+    }
+
+
+    constructor() {
+        super();
+        this.placeholder = this.placeholder || "Default placeholder...";
+
+        // Render when appropriate
+        this.render();
+
+        // i.e., this[name] = this.shadowRoot.getElementById(name)
+        this.select("list", "new-todo");
+        this.bind();
+    }
+
+    bind() {
+        this.addEventListener("keyup", (e) => {
+            switch (e.key) {
+            case "Enter":
+                this.$["new-todo"].value && this.addItem({
+                        name: this.$["new-todo"].value,
+                        state: "todo"
+                });
+                this.$["new-todo"].value = null;
+                break;
+            }
+        });
+
+        this.addEventListener("item-deleted", (e) => {
+            this.$["list"].removeChild(e.detail.item);
+        });
+    }
+
+    addItem(item) {
+        this.$["list"].appendChild(new TodoItem(item));
+    }
+
+    get template() {
+        return (
+            ["div",
+             ["input", {id: "new-todo", type: "text", placeholder: this.placeholder}],
+             ["ul", {id: "list"}, null]]
+        );
+    }
+
+    get styles() {
+        return `
+            #list { margin: 0; padding: 0; }
+            #new-todo { font-size: 2rem; margin: 0; padding: 0; width: 100%; }
+        `;
+    }
+
+}
+
+window.customElements.define("todo-list", TodoList);
+```
+
+```html
+<!doctype html>
+<html>
+<head><script type="module" src="example.js"></script></head>
+<body>
+    <todo-list placeholder="what do you want to do today?"></todo-list>
+</body>
+</html>
+```
+
 You can see an example todo app using the web component mixin by running the "demo" script from the top level of the repo using npm/yarn.
 
 ```bash
-$ yarn install && yarn demo
+$ npm install && npm demo
 ```
 
--- example.js
+++ example.js
@@ -4,137 +4,144 @@ const states = ["todo", "doing", "done"];
 
 
 class TodoItem extends Hoquet(HTMLElement) {
-  constructor({name, state}) {
-    super();
-    this.name = name;
-    this.render();
+    constructor({name, state}) {
+        super();
+        this.name = name;
+        this.render();
 
-    this.select(
-        "x",
-        ["$li", "li", "querySelector"]
-    );
+        this.select(
+                "x",
+                ["$li", "li", "querySelector"]
+        );
 
-    this.state = state;
+        this.state = state;
 
-    this.$li.addEventListener("click", e => this.toggleState());
-    this.$["x"].addEventListener("click", e => {
-      e.stopPropagation();
-      this.dispatchEvent(new CustomEvent("item-deleted", {
-        composed: true, bubbles: true, detail: {}
-      }));
-    });
-  }
+        this.$li.addEventListener("click", e => this.toggleState());
+        this.$["x"].addEventListener("click", e => {
+            e.stopPropagation();
+            this.dispatchEvent(new CustomEvent("item-deleted", {
+                composed: true, bubbles: true, detail: {item: this}
+            }));
+        });
+    }
 
-  get state() { return this._state }
-  set state(state) {
-    this._state = state;
-    states.forEach(x => this.$li.classList.remove(x));
-    this.$li.classList.add(state);
-  }
+    get state() { return this._state }
+    set state(state) {
+        this._state = state;
+        states.forEach(x => this.$li.classList.remove(x));
+        this.$li.classList.add(state);
+    }
 
-  toggleState() {
-    const currentStateIndex = states.indexOf(this.state);
-    this.state = states[
-      currentStateIndex >= states.length - 1
-        ? 0 : currentStateIndex + 1
-    ];
-  }
+    toggleState() {
+        const currentStateIndex = states.indexOf(this.state);
+        this.state = states[
+            currentStateIndex >= states.length - 1
+                ? 0 : currentStateIndex + 1
+        ];
+    }
 
-  get template() {
-    return (
-      ["li"
-      , ["span", {class: "name"}, this.name]
-      , ["span", {id: "x"}, "x"]
-      ]
-    );
-  }
+    get template() {
+        return (
+            ["li"
+            , ["span", {class: "name"}, this.name]
+            , ["span", {id: "x"}, "x"]
+            ]
+        );
+    }
 
-  get styles() {
-    return `
-      li {
-        cursor: pointer;
-        list-style: none;
-        padding: 10px 15px;
-        background-color: #fafafa;
-        border: 1px solid #ff0;
-        margin: 5px 0;
-        font-family: sans-serif;
-        font-size: 2rem;
-      }
-      .todo .name { color: #33a; }
+    get styles() {
+        return `
+            li {
+                cursor: pointer;
+                list-style: none;
+                padding: 10px 15px;
+                background-color: #fafafa;
+                border: 1px solid #ff0;
+                margin: 5px 0;
+                font-family: sans-serif;
+                font-size: 2rem;
+            }
+            .todo .name { color: #33a; }
 
-      .doing .name { color: orange }
-      .doing .name::after { content: " (in progress)" }
+            .doing .name { color: orange }
+            .doing .name::after { content: " (in progress)" }
 
-      .done .name {
-        text-decoration: line-through;
-        color: #888;
-      }
-      .name {
-        width: 75%;
-      }
-      #x {
-        text-decoration: none!important;
-        display: block;
-        float: right;
-        background-color: #fff;
-        padding: 0 10px;
-        font-size: 2rem;
-      }
-    `;
-  }
+            .done .name {
+                text-decoration: line-through;
+                color: #888;
+            }
+            .name {
+                width: 75%;
+            }
+            #x {
+                text-decoration: none!important;
+                display: block;
+                float: right;
+                background-color: #fff;
+                padding: 0 10px;
+                font-size: 2rem;
+            }
+        `;
+    }
 }
 
 
 class TodoList extends Hoquet(HTMLElement) {
 
-  static get reflectedAttributes() { return ["placeholder"]; }
+    static get reflectedAttributes() { return ["placeholder"]; }
+    static get observedAttributes() { return ["placeholder"]; }
+    attributeChangedCallback(key, prev, curr) {
+        if (key === "placeholder") {
+            this.$["new-todo"].placeholder = curr;
+        }
+    }
 
-  constructor() {
-    super();
-    this.placeholder = this.placeholder || "Default placeholder...";
-    this.render();
-    this.select("list", "new-todo");
-    this.bind();
-  }
+    constructor() {
+        super();
+        this.placeholder = this.placeholder || "Default placeholder...";
+        console.log(this.placeholder);
+        this.render();
+        this.select("list", "new-todo");
+        this.bind();
+    }
 
-  bind() {
-    this.addEventListener("keyup", (e) => {
-      switch (e.key) {
-      case "Enter":
-        this.$["new-todo"].value && this.addItem({
-            name: this.$["new-todo"].value,
-            state: "todo"
+    bind() {
+        this.addEventListener("keyup", (e) => {
+            switch (e.key) {
+            case "Enter":
+                this.$["new-todo"].value && this.addItem({
+                        name: this.$["new-todo"].value,
+                        state: "todo"
+                });
+                this.$["new-todo"].value = null;
+                break;
+            }
         });
-        this.$["new-todo"].value = null;
-        break;
-      }
-    });
 
-    this.shadowRoot.addEventListener("item-deleted", (e) => {
-      this.$["list"].removeChild(e.target);
-    });
-  }
+        this.addEventListener("item-deleted", (e) => {
+            this.$["list"].removeChild(e.detail.item);
+        });
+    }
 
-  addItem(item) {
-    this.$["list"].appendChild(new TodoItem(item));
-  }
+    addItem(item) {
+        this.$["list"].appendChild(new TodoItem(item));
+    }
 
-  get template() {
-    return (
-      ["div"
-      , ["input", {id: "new-todo", type: "text", placeholder: this.placeholder}]
-      , ["ul", {id: "list"}, null]
-      ]
-    );
-  }
+    get template() {
+        return (
+            ["div"
+            , ["input", {id: "new-todo", type: "text", placeholder: this.placeholder}]
+            , ["ul", {id: "list"}, null]
+            ]
+        );
+    }
 
-  get styles() {
-    return `
-      #list { margin: 0; padding: 0; }
-      #new-todo { font-size: 2rem; margin: 0; padding: 0; width: 100%; }
-    `;
-  }
+    get styles() {
+        return `
+            #list { margin: 0; padding: 0; }
+            #new-todo { font-size: 2rem; margin: 0; padding: 0; width: 100%; }
+        `;
+    }
 
 }
 
