69163bad357aad6322e232cb51bc58f97de9573f
Tom Brennan <tjb1982@gmail.com>
2020-06-13 11:40:08 -0400

    enumerable reflected attrs

--- mixin.js
+++ mixin.js
@@ -35,7 +35,8 @@ export default ((C = null, {shadowy = true} = {}) => {
                     },
                     set: (value) => {
                         this.setAttribute(k, value);
-                    }
+                    },
+                    enumerable: true
                 });
             });
         }
